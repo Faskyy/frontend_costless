1a42ff119c1c4a4a93d0febc6ee5d4b45db39cd1
Faskyy <fahd.ksara1@gmail.com>
2023-06-11 19:54:58 -0400

    changed endpoint

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -51,7 +51,7 @@ function MapComponent() {
   };
 
   useEffect(() => {
-    fetch('http://127.0.0.1:5000/events')
+    fetch('https://costless.herokuapp.com/events')
       .then((response) => {
         if (!response.ok) {
           throw new Error('Network response was not ok.');
