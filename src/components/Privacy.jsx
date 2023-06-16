408baf3b79b6dd957f7cc820b337c61ece45e5e0
Faskyy <fahd.ksara1@gmail.com>
2023-06-16 11:41:49 -0400

    testing something

--- src/components/Privacy.jsx
+++ src/components/Privacy.jsx
@@ -1,97 +1,97 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
-function Privacy() {
-  return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        minHeight: '100vh',
-        fontFamily: 'Arial, sans-serif',
-        fontSize: '12px',
-        textAlign: 'center',
-        backgroundColor: 'grey',
-      }}
-    >
-    <br />
-      <Link to="/" style={{ textDecoration: 'none', marginBottom: '16px', position: 'sticky', top: '16px', zIndex: '1' }}>
-        <button className="btn btn-light btn-sm">
-          <FontAwesomeIcon icon={faArrowLeft} style={{ marginRight: '8px' }} />Back
-        </button>
-      </Link>
-      <div
-        style={{
-          maxWidth: '800px',
-          textAlign: 'left',
-          backgroundColor: 'white',
-          padding: '70px',
-          borderRadius: '4px',
-          overflowY: 'auto',
-          maxHeight: 'calc(100vh - 100px)', // Adjust the value based on your needs
-        }}
-      >
-        <h1 style={{ fontSize: '24px', marginBottom: '16px', fontWeight: 'bold' }}>Privacy Policy</h1>
-        <p style={{ marginBottom: '16px' }}>
-          <strong>Effective Date:</strong> June 16, 2023
-        </p>
-        <h2 style={{ fontSize: '18px', marginBottom: '8px', fontWeight: 'bold' }}>1. Information We Collect</h2>
-        <p style={{ marginBottom: '16px' }}>
-          <strong>1.1 Personal Information:</strong> When you create an account on Costless, we collect certain personal
-          information such as your name, email address, and any other information you provide during the registration process.
-        </p>
-        <p style={{ marginBottom: '16px' }}>
-          <strong>1.2 Log Data:</strong> Our servers automatically record certain information ("Log Data") when you use Costless.
-          This Log Data may include your IP address, browser type, operating system, referring URLs, access times, and other statistics.
-        </p>
-        <h2 style={{ fontSize: '18px', marginBottom: '8px', fontWeight: 'bold' }}>2. How We Use Your Information</h2>
-        <p style={{ marginBottom: '16px' }}>
-          <strong>2.1 Headcount and Analytics:</strong> The primary purpose of collecting your information is to maintain a headcount of the users accessing our web application.
-          This data helps us understand the size of our user base and analyze usage patterns to improve our services.
-        </p>
-        <p style={{ marginBottom: '16px' }}>
-          <strong>2.2 Communication:</strong> We may use your email address to communicate with you about important updates, announcements, or changes to our services.
-          You can opt-out of receiving these communications by following the instructions provided in the emails.
-        </p>
-        <p style={{ marginBottom: '16px' }}>
-          <strong>2.3 Aggregated Data:</strong> We may aggregate and anonymize the information collected to generate statistical and analytical insights.
-          Such aggregated data will not personally identify you and may be used for various purposes, including research, marketing, and improving our services.
-        </p>
-        <h2 style={{ fontSize: '18px', marginBottom: '8px', fontWeight: 'bold' }}>3. Data Security</h2>
-        <p style={{ marginBottom: '16px' }}>
-          We take reasonable measures to protect the information collected from unauthorized access, disclosure, alteration, or destruction.
-          However, please note that no data transmission over the internet or storage system can be guaranteed to be 100% secure.
-        </p>
-        <h2 style={{ fontSize: '18px', marginBottom: '8px', fontWeight: 'bold' }}>4. Data Sharing and Disclosure</h2>
-        <p style={{ marginBottom: '16px' }}>
-          <strong>4.1 Third-Party Service Providers:</strong> We may engage trusted third-party service providers to assist us in delivering and improving our services.
-          These providers may have access to your personal information solely for the purpose of performing tasks on our behalf and are obligated not to disclose or use it for any other purpose.
-        </p>
-        <p style={{ marginBottom: '16px' }}>
-          <strong>4.2 Legal Requirements:</strong> We may disclose your information if required to do so by law or in the good faith belief that such action is necessary to comply with legal obligations,
-          protect and defend our rights or property, or investigate potential violations of our terms of service.
-        </p>
-        <h2 style={{ fontSize: '18px', marginBottom: '8px', fontWeight: 'bold' }}>5. Your Choices and Rights</h2>
-        <p style={{ marginBottom: '16px' }}>
-          <strong>5.1 Access, Correct, and Delete:</strong> You have the right to access, correct, or delete your personal information.
-          You can manage your account settings or contact us at <strong>hello@costless.app</strong> to request access to your information or make any necessary changes.
-        </p>
-        <h2 style={{ fontSize: '18px', marginBottom: '8px', fontWeight: 'bold' }}>6. Changes to this Privacy Policy</h2>
-        <p style={{ marginBottom: '16px' }}>
-          We reserve the right to modify this Privacy Policy at any time. Any changes will be effective immediately upon posting the updated policy on our website.
-          It is your responsibility to review this Privacy Policy periodically for any updates.
-        </p>
-        <h2 style={{ fontSize: '18px', marginBottom: '8px', fontWeight: 'bold' }}>7. Contact Us</h2>
-        <p style={{ marginBottom: '16px' }}>
-          If you have any questions, concerns, or suggestions regarding this Privacy Policy, please contact us at <strong>hello@costless.app</strong>
-        </p>
-      </div>
-    </div>
-  );
-}
+// function Privacy() {
+//   return (
+//     <div
+//       style={{
+//         display: 'flex',
+//         flexDirection: 'column',
+//         justifyContent: 'flex-start',
+//         alignItems: 'center',
+//         minHeight: '100vh',
+//         fontFamily: 'Arial, sans-serif',
+//         fontSize: '12px',
+//         textAlign: 'center',
+//         backgroundColor: 'grey',
+//       }}
+//     >
+//     <br />
+//       <Link to="/" style={{ textDecoration: 'none', marginBottom: '16px', position: 'sticky', top: '16px', zIndex: '1' }}>
+//         <button className="btn btn-light btn-sm">
+//           <FontAwesomeIcon icon={faArrowLeft} style={{ marginRight: '8px' }} />Back
+//         </button>
+//       </Link>
+//       <div
+//         style={{
+//           maxWidth: '800px',
+//           textAlign: 'left',
+//           backgroundColor: 'white',
+//           padding: '70px',
+//           borderRadius: '4px',
+//           overflowY: 'auto',
+//           maxHeight: 'calc(100vh - 100px)', // Adjust the value based on your needs
+//         }}
+//       >
+//         <h1 style={{ fontSize: '24px', marginBottom: '16px', fontWeight: 'bold' }}>Privacy Policy</h1>
+//         <p style={{ marginBottom: '16px' }}>
+//           <strong>Effective Date:</strong> June 16, 2023
+//         </p>
+//         <h2 style={{ fontSize: '18px', marginBottom: '8px', fontWeight: 'bold' }}>1. Information We Collect</h2>
+//         <p style={{ marginBottom: '16px' }}>
+//           <strong>1.1 Personal Information:</strong> When you create an account on Costless, we collect certain personal
+//           information such as your name, email address, and any other information you provide during the registration process.
+//         </p>
+//         <p style={{ marginBottom: '16px' }}>
+//           <strong>1.2 Log Data:</strong> Our servers automatically record certain information ("Log Data") when you use Costless.
+//           This Log Data may include your IP address, browser type, operating system, referring URLs, access times, and other statistics.
+//         </p>
+//         <h2 style={{ fontSize: '18px', marginBottom: '8px', fontWeight: 'bold' }}>2. How We Use Your Information</h2>
+//         <p style={{ marginBottom: '16px' }}>
+//           <strong>2.1 Headcount and Analytics:</strong> The primary purpose of collecting your information is to maintain a headcount of the users accessing our web application.
+//           This data helps us understand the size of our user base and analyze usage patterns to improve our services.
+//         </p>
+//         <p style={{ marginBottom: '16px' }}>
+//           <strong>2.2 Communication:</strong> We may use your email address to communicate with you about important updates, announcements, or changes to our services.
+//           You can opt-out of receiving these communications by following the instructions provided in the emails.
+//         </p>
+//         <p style={{ marginBottom: '16px' }}>
+//           <strong>2.3 Aggregated Data:</strong> We may aggregate and anonymize the information collected to generate statistical and analytical insights.
+//           Such aggregated data will not personally identify you and may be used for various purposes, including research, marketing, and improving our services.
+//         </p>
+//         <h2 style={{ fontSize: '18px', marginBottom: '8px', fontWeight: 'bold' }}>3. Data Security</h2>
+//         <p style={{ marginBottom: '16px' }}>
+//           We take reasonable measures to protect the information collected from unauthorized access, disclosure, alteration, or destruction.
+//           However, please note that no data transmission over the internet or storage system can be guaranteed to be 100% secure.
+//         </p>
+//         <h2 style={{ fontSize: '18px', marginBottom: '8px', fontWeight: 'bold' }}>4. Data Sharing and Disclosure</h2>
+//         <p style={{ marginBottom: '16px' }}>
+//           <strong>4.1 Third-Party Service Providers:</strong> We may engage trusted third-party service providers to assist us in delivering and improving our services.
+//           These providers may have access to your personal information solely for the purpose of performing tasks on our behalf and are obligated not to disclose or use it for any other purpose.
+//         </p>
+//         <p style={{ marginBottom: '16px' }}>
+//           <strong>4.2 Legal Requirements:</strong> We may disclose your information if required to do so by law or in the good faith belief that such action is necessary to comply with legal obligations,
+//           protect and defend our rights or property, or investigate potential violations of our terms of service.
+//         </p>
+//         <h2 style={{ fontSize: '18px', marginBottom: '8px', fontWeight: 'bold' }}>5. Your Choices and Rights</h2>
+//         <p style={{ marginBottom: '16px' }}>
+//           <strong>5.1 Access, Correct, and Delete:</strong> You have the right to access, correct, or delete your personal information.
+//           You can manage your account settings or contact us at <strong>hello@costless.app</strong> to request access to your information or make any necessary changes.
+//         </p>
+//         <h2 style={{ fontSize: '18px', marginBottom: '8px', fontWeight: 'bold' }}>6. Changes to this Privacy Policy</h2>
+//         <p style={{ marginBottom: '16px' }}>
+//           We reserve the right to modify this Privacy Policy at any time. Any changes will be effective immediately upon posting the updated policy on our website.
+//           It is your responsibility to review this Privacy Policy periodically for any updates.
+//         </p>
+//         <h2 style={{ fontSize: '18px', marginBottom: '8px', fontWeight: 'bold' }}>7. Contact Us</h2>
+//         <p style={{ marginBottom: '16px' }}>
+//           If you have any questions, concerns, or suggestions regarding this Privacy Policy, please contact us at <strong>hello@costless.app</strong>
+//         </p>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default Privacy;
+// export default Privacy;
